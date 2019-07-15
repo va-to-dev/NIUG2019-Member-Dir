64b1c1e367242e191e50d8b596228db63528527f
Visual Antidote <41328371+va-to@users.noreply.github.com>
2019-07-15 14:07:58 -0600

    remove bearer and URLs

--- src/app/conomember-list/localMember.service.ts
+++ src/app/conomember-list/localMember.service.ts
@@ -17,12 +17,13 @@ export class LocalMemberService {
 //private remoteService: RemoteService
   getMembers(): Observable<Member[]> {
    
-    //let baseURL = 'https://portalapi.visualantidote.com/TestPortal';
-    let baseURL = 'https://portalapi.visualantidote.com/Asi.Scheduler_TestPortal';
+    //let baseURL = 'https://www.domain.com/iMIS';
+    let baseURL = 'https://api.domain.com/Asi.Scheduler_IMIS';
+    
     let url = baseURL + '/api/IQA?QueryName=$/ContactManagement/DefaultSystem/Queries/Contacts/Default&limit=1000';
 
-   let authToken = 'bearer 3hFYeMWKVbdY8tWX6YxUV3onbrm08IkEMQFh3QLL56KGrClp21lNpJXcXQ-HFNhiNzgKXlQ6d_vWHR6zwqdnSLT0VOVjGZPUlFrevtf4Xbc7Ile5yZD8lcsOtyhgFoECMcu0AQsVlrU32e1MtDvt9c1GQBXDrv6oy03VCg5Ff3zHVlLargQtBGAxyDOPH0MjMqAukMsmypZW8RuWKVwdgq4vG2xHKug7lMl33n7rJaBCOBdmtSM5dqJR-owO_5hwMO-aYZ7lMmPItqMMZlhpBSEQ26KUdZt-vS2vIL5L6EJDCe_WAmJTpjlSaJi7OfJYVFfhi3A2waHyK52EOJnWAoP9-dJTGEz3nMX3Pb_mcAmvaE-Fb3ck3Kea99zrEweU_rKofc9C9A9wbyL8aUQrHYSfD-Zlmdd_mZvvesnta7SmFVhXf5_LMZ1fW5OloBfYheFVZbfTmEr3G8LGGGRPdN3SQSlVF8by4_GxCvLNmtyvOnPLcPBVFCEQrxA_GUoZ2YG4s4TMkRwyWXR0gS8U__i3SlSZxPwBB6HaRFKeC5h5y5CwFrAhlniBYdUbLPS4b9MOPRjuJGJHZg1NZc8ot3EqQhh00KJ04v7IxW1d9DnhDA5xVmZELuNFWgEvyJOtSrwBl8Kv6kUaN91eWVxe4SBLSP9TMJ1HjQaEFLDrfaEautiEfera-rAH4U972RfaLt0z5dsyBrhuTu_ZuC_-mvAq6BuYIzoP3MIYTmo2HWRGGo4oo6bYXIhi_nMmhqnnbORppaz1MISMXE0NG8JEMObbG3lXU3BwYPqE-xOZYIM81eVaDH4rb5XtfKIYa4Lfr63AZeU7bElVRrz0A0RWAoHE-XtTDULT_EkyBmivE7QTo_oNVg05iefzs0xQm9qBm7zdR_rT1vxqhmZ4QgGkuoc8m5mKUmA-GUvP1j-q4Pxpm6aOZYpmPygMzQIEdxs1wrJc_yWpaKyR9DA6NAnpx_IIGRXUiMiQG9WJOxJCQQG0k7IIP7N-Dgfw2T2w7V9R84yq4SlKFD34_v5SVx5MYg';
-   // let authToken = (document.getElementById('__RequestVerificationToken') as HTMLInputElement).value;
+   //let authToken = 'bearer ....';
+    let authToken = (document.getElementById('__RequestVerificationToken') as HTMLInputElement).value;
   
       return this.http.get(url, {headers: { 'Authorization': authToken }})
         .map((res:any)=> {
